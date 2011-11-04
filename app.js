dd96c78abcde8e6156cf5b27de4a4ab99a31c86f
Peter Wooley <peterwooley@gmail.com>
2011-11-03 19:22:40 -0700

    Recoved what I could from Cloud9 and added in a Box2DWeb demo.

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ var express = require('express'),
 	port = process.env.C9_PORT || process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
+//app.set('view engine', 'jade');
+//app.set('view options', { layout: false });
+//app.set('jsonp callback', 'callback');
+
+// Route the home page to views/index.jade
+//app.get('/', function(req, res){
+//    res.render('index');
+//});
+
 app.listen(port);
